a3e636e057c7c1854b8653622e52b0d9fbbcb255
Manjunath Davanam <manjunath@sanketika.in>
2022-12-15 18:45:27 +0530

    #OB-45 feat: Generation of the signed url for the relative path

--- analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/JobAPIService.scala
+++ analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/JobAPIService.scala
@@ -377,8 +377,11 @@ class JobAPIService @Inject()(postgresDBUtil: PostgresDBUtil, apiValidator: APIV
         val values = f.split("/").toList.drop(4) // 4 - is derived from 2 -> '//' after http, 1 -> uri and 1 -> container
         values.mkString("/")
       }
-      else {
+      else if(f.contains("wasb")) {
         val values = f.split("/").toList.drop(3) // 3 - is derived from 2 -> '//' after wasb, 1 -> uri
+        values.mkString("/")
+      } else{
+        val values = f.split("/").toList
         values.mkString("/")
       }
       APILogger.log("Getting signed URL for - " + objectKey)
